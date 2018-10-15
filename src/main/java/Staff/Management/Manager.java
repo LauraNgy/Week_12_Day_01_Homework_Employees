6128d62d4623da25df781a77b562658578178b4e
Laura Nagy <ngylaura@gmail.com>
2018-10-15 16:53:41 +0100

    Adds and tests Manager class

--- src/main/java/Staff/Management/Manager.java
+++ src/main/java/Staff/Management/Manager.java
@@ -3,4 +3,15 @@ package Staff.Management;
 import Staff.Employee;
 
 public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String NiNo, double salary, String deptName) {
+        super(name, NiNo, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,48 @@
+import Staff.Employee;
+import Staff.Management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Employee employee;
+
+    @Before
+    public void before() {
+        employee = new Manager("Billy Joe Jimbob", "AR374902S", 40000.00, "Edinburgh");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Billy Joe Jimbob", employee.getName());
+    }
+
+    @Test
+    public void hasNINo() {
+        assertEquals("AR374902S", employee.getNINo());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(40000.00, employee.getSalary(), 0.1);
+    }
+
+    @Test
+    public void hasDeptName() {
+        Manager manager = (Manager) employee;
+        assertEquals("Edinburgh", manager.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        employee.raiseSalary(5000.50);
+        assertEquals(45000.50, employee.getSalary(), 0.1);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(400.00, employee.payBonus(), 0.1);
+    }
 }
