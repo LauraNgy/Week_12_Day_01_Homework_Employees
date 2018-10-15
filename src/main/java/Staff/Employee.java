c1851dd3392b91589e2871c46ede8b977c4fed53
Laura Nagy <ngylaura@gmail.com>
2018-10-15 16:04:45 +0100

    Adds Employee class

--- src/main/java/Staff/Employee.java
+++ src/main/java/Staff/Employee.java
@@ -1,4 +1,34 @@
 package Staff;
 
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private String NINo;
+    private double salary;
+
+    public Employee(String name, String NiNo, double salary) {
+        this.name = name;
+        this.NINo = NiNo;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNINo() {
+        return this.NINo;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double raise) {
+        this.salary += raise;
+    }
+
+    public double payBonus() {
+        return this.salary/100;
+    }
 }
